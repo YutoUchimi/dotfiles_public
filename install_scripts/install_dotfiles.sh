37e45a1a3f6b27a9c4d545d59f78348b5d9352cb
Yuto Uchimi <momosama1072@gmail.com>
2018-06-26 17:46:48 +0900

    Fix install_dotfiles.sh not to install non-existent files

--- install_scripts/install_dotfiles.sh
+++ install_scripts/install_dotfiles.sh
@@ -9,18 +9,11 @@ cp -r \
    ${REPOSITORY_PATH}/.aspell.conf \
    ${REPOSITORY_PATH}/.bash_aliases \
    ${REPOSITORY_PATH}/.bashrc \
-   ${REPOSITORY_PATH}/.crontab \
    ${REPOSITORY_PATH}/.emacs.d \
-   ${REPOSITORY_PATH}/.gitconfig \
    ${REPOSITORY_PATH}/.gitignore_global \
-   ${REPOSITORY_PATH}/.gpu_checker.cfg \
    ${REPOSITORY_PATH}/.local \
    ${REPOSITORY_PATH}/.notify_time.sh \
    ${REPOSITORY_PATH}/.tmux.conf \
    $HOME
-
-cp -r \
-   ${REPOSITORY_PATH}/usr \
-   /usr
 
 set +x
