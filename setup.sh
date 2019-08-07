73a464c8a210665f846e2173cfc633bf991b4727
Yuto Uchimi <momosama1072@gmail.com>
2019-08-08 08:58:10 +0900

    Add apt/pip requirements and install them in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,17 @@
 
 set_cmd_for_sudo () {
     if [ $(whoami) = 'root' ]; then
+        apt_get_cmd='apt-get'
+        pip_cmd='pip'
+        sed_cmd='sed'
         sensors_detect_cmd='sensors-detect'
         service_cmd='service'
         tee_cmd='tee'
         touch_cmd='touch'
     else
+        apt_get_cmd='sudo apt-get'
+        pip_cmd='sudo -H pip'
+        sed_cmd='sudo sed'
         sensors_detect_cmd='sudo sensors-detect'
         service_cmd='sudo service'
         tee_cmd='sudo tee'
@@ -15,26 +21,43 @@ set_cmd_for_sudo () {
 }
 
 
+apt_get_install () {
+    local repo_path=$(echo $(cd $(dirname $0) && pwd))
+    $apt_get_cmd update
+    $apt_get_cmd install \
+                 $(grep -vE "^\s*#" ${repo_path}/apt_requirements.txt | \
+                       tr "\n" " ")
+}
+
+
+pip_install () {
+    local repo_path=$(echo $(cd $(dirname $0) && pwd))
+    $pip_cmd install "pip==9.0.3"
+    $pip_cmd install -r ${repo_path}/pip_requirements.txt
+}
+
+
 copy_dotfiles () {
-    REPOSITORY_PATH=$(echo $(cd $(dirname $0) && pwd))
+    local repo_path=$(echo $(cd $(dirname $0) && pwd))
 
     if [ -f ${HOME}/.bashrc -a ! -f ${HOME}/.bashrc.orig ]; then
         mv $HOME/.bashrc $HOME/.bashrc.orig
     fi
 
     cp -r \
-       ${REPOSITORY_PATH}/.Xmodmap \
-       ${REPOSITORY_PATH}/.aspell.conf \
-       ${REPOSITORY_PATH}/.bash_aliases \
-       ${REPOSITORY_PATH}/.bashrc \
-       ${REPOSITORY_PATH}/.cron_functions.sh \
-       ${REPOSITORY_PATH}/.crontab \
-       ${REPOSITORY_PATH}/.emacs.d \
-       ${REPOSITORY_PATH}/.gitconfig \
-       ${REPOSITORY_PATH}/.gitignore_global \
-       ${REPOSITORY_PATH}/.local \
-       ${REPOSITORY_PATH}/.rviz \
-       ${REPOSITORY_PATH}/.tmux.conf \
+       ${repo_path}/.Xmodmap \
+       ${repo_path}/.aspell.conf \
+       ${repo_path}/.bash_aliases \
+       ${repo_path}/.bashrc \
+       ${repo_path}/.cron_functions.sh \
+       ${repo_path}/.crontab \
+       ${repo_path}/.emacs.d \
+       ${repo_path}/.gitconfig \
+       ${repo_path}/.gitignore_global \
+       ${repo_path}/.gpu_checker.cfg \
+       ${repo_path}/.local \
+       ${repo_path}/.rviz \
+       ${repo_path}/.tmux.conf \
        $HOME
 }
 
@@ -46,10 +69,10 @@ home_dir_name_ja2en () {
 
 
 setup_keybind () {
-    func_file=/usr/share/X11/xkb/symbols/underscore
+    local func_file=/usr/share/X11/xkb/symbols/underscore
     $touch_cmd $func_file
     grep nobackslash $func_file > /dev/null
-    exit_status=$?
+    local exit_status=$?
     if [ $exit_status -eq 1 ]; then
         echo 'partial modifier_keys' | $tee_cmd $func_file
         echo 'xkb_symbols "nobackslash" {' | $tee_cmd -a $func_file
@@ -58,9 +81,9 @@ setup_keybind () {
         echo '};' | $tee_cmd -a $func_file
     fi
 
-    rules_file=/usr/share/X11/xkb/rules/evdev
+    local rules_file=/usr/share/X11/xkb/rules/evdev
     grep nobackslash $rules_file > /dev/null
-    exit_status=$?
+    local exit_status=$?
     if [ $exit_status -eq 1 ]; then
         echo '! option = symbols' | $tee_cmd -a $rules_file
         echo '  underscore:nobackslash = +underscore(nobackslash)' | \
@@ -69,6 +92,22 @@ setup_keybind () {
 
     dconf write /org/gnome/desktop/input-sources/xkb-options \
           "['ctrl:nocaps', 'underscore:nobackslash']"
+}
+
+
+setup_ssh_daemon () {
+    local config_file=/etc/ssh/sshd_config
+    $sed_cmd \
+        -i \
+        -e "s/$(cat $config_file | \
+               grep -E ^#\?PermitRootLogin)/PermitRootLogin no/" \
+        $config_file
+    $sed_cmd \
+        -i \
+        -e "s/$(cat $config_file | \
+               grep -E ^#\?PasswordAuthentication)/PasswordAuthentication no/" \
+        $config_file
+    $service_cmd ssh restart
 }
 
 
@@ -91,9 +130,12 @@ setup_lm_sensors () {
 
 set -x
 set_cmd_for_sudo
+apt_get_install
+pip_install
 copy_dotfiles
 home_dir_name_ja2en
 setup_keybind
+setup_ssh_daemon
 setup_ntp_daemon
 setup_lm_sensors
 set +x
