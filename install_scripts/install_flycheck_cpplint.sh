cac272ca127b25fbec0c6ed582573d32de5aeccd
Yuto Uchimi <momosama1072@gmail.com>
2019-08-08 09:03:34 +0900

    cpplint scripts in ~/.local, not /usr/local

--- install_scripts/install_flycheck_cpplint.sh
+++ install_scripts/install_flycheck_cpplint.sh
@@ -2,11 +2,11 @@
 
 set -x
 wget https://raw.githubusercontent.com/google/styleguide/gh-pages/cpplint/cpplint.py
-sudo mv cpplint.py /usr/local/bin/cpplint.py
-sudo chmod 755 /usr/local/bin/cpplint.py
+mv cpplint.py $HOME/.local/bin/cpplint.py
+chmod 755 $HOME/.local/bin/cpplint.py
 set +x
 
-if [ -f /usr/local/bin/cpplint.py ]; then
+if [ -f $HOME/.local/bin/cpplint.py ]; then
     echo "\n\033[01;36m[$(basename $0)] Successfully installed! cpplint.py is installed.\033[00m\n"
     notify-send -i gnome-terminal -t 3000 -u normal \
                 "Successfully installed!" "cpplint.py is installed."
@@ -14,7 +14,7 @@ fi
 
 set -x
 REPOSITORY_PATH=$(echo $(cd $(dirname $0)/.. && pwd))
-sudo cp ${REPOSITORY_PATH}/.local/lib/flycheck-google-cpplint.el ${HOME}/.local/lib/flycheck-google-cpplint.el
+cp ${REPOSITORY_PATH}/.local/lib/flycheck-google-cpplint.el ${HOME}/.local/lib/flycheck-google-cpplint.el
 set +x
 
 if [ -f ${HOME}/.local/lib/flycheck-google-cpplint.el ]; then
